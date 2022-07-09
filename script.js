f3fa71305d8644e1d28495983f0b461381eaaf7c
Luiz Felipe <luizfelipealvesdesouza706@gmail.com>
2022-07-08 23:20:46 -0300

    defesa adicionada

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var vida2 = 100
-var vida1 = 100
+let vida2 = 100
+let vida1 = 100
+var vidajiren = window.document.getElementById("vidajiren")
+var vidagoku = window.document.getElementsByClassName("vidagoku")[0]
+var barra_super = 0
+
 function attack() {
-    let vidajiren = window.document.getElementById("vidajiren")
-    let vidagoku = window.document.getElementsByClassName("vidagoku")[0]
     vida1 -= 9
     vida2 -= 10
     vidajiren.innerHTML = `${vida2}`
@@ -11,5 +13,19 @@ function attack() {
         window.alert("Você venceu!")
         vida2 = 100
         vida1 = 100
-    }
+    } 
+}
+
+function defesa() {
+    vida1 -= 2
+    vidagoku.innerHTML = `${vida1}` 
+    if (vida2 == 0) {
+        window.alert("Você venceu!")
+        vida2 = 100
+        vida1 = 100
+    } 
+}
+
+function supe() {
+
 }
